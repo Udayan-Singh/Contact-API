706bd1dbbc4557ca80fdd9400e774ab0b2c1d468
udayan singh <udayans0107@gmail.com>
2023-06-21 23:48:06 +0530

    Before Docker

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
 const connectDb = require('./config/dbConnection');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv').config();
-const app = express();
 
+
+const app = express();
+app.use(cookieParser());
 connectDb();
 
 const port = process.env.PORT || 5000;
@@ -13,6 +16,11 @@ app.use('/api/students', require('./routes/contactRoutes'));
 app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler);
+
+app.get("/", (req,res) => {
+    res.send("Hello")
+})
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
